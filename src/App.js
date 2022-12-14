47f51243d18fc0c19744365b8916ce8c4456e91f
7385929773 <manepriya222019@gmail.com>
2022-12-14 22:11:15 +0530

    React Project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import React from 'react'
+import './App.css';
+//import Products from "./component/Products"
+import Cart from "./component/Cart"
+
+
+function App() {
+  return (
+    <div className="App">
+   <Cart />
+</div>
+  );
+  
+
+}
+
+export default App;
+
+
